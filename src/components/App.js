f436ae4c5d48d63c6b37249507854987b5b45244
BillClinton <bclinton@appunto.net>
2019-06-10 21:05:34 -0400

    Handle preset actions by calling hue api

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
 import React from "react";
 import PresetButtonList from "./PresetButtonList";
+import HueLocal from "../apis/HueLocal";
+import Presets from "../apis/Presets.js";
 import "./App.css";
 
 class App extends React.Component {
+  //onPresetSelect = async preset => {
+  onPresetSelect = preset => {
+    const actions = [];
+
+    preset.states.forEach(state => {
+      let action = state.type === "light" ? "state" : "action";
+      let url = "/" + state.type + "s/" + state.id + "/" + action;
+
+      actions.push(
+        new Promise(() => {
+          return HueLocal.put(url, state.body);
+        })
+      );
+    });
+
+    Promise.all(actions)
+      .then(vals => {
+        console.log("promise");
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
+
   render() {
     return (
       <div className="app">
         <div className="ui one column centered grid">
           <div className="eight wide column" style={{ marginTop: "10px" }}>
             <PresetButtonList
-              presets={[
-                {
-                  name: "movies"
-                },
-                {
-                  name: "bedtime"
-                }
-              ]}
+              onPresetSelect={this.onPresetSelect}
+              presets={Presets}
             />
           </div>
         </div>
